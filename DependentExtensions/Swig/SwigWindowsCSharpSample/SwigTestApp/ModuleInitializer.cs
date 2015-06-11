aa2895329041226d2a25a163b197017586151786
jmderuty <jmderuty@stormancer.com>
2015-06-11 16:48:35 +0200

    Added dynamic loading of Raknet to work with AnyCPU assemblies as part of the swig dll (see DependentExtensions\Swig\SwigWindowsCSharpSample)

---
+++ DependentExtensions/Swig/SwigWindowsCSharpSample/SwigTestApp/ModuleInitializer.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Used by the ModuleInit. All code inside the Initialize method is ran as soon as the assembly is loaded.
+/// </summary>
+using System;
+using System.Runtime.InteropServices;
+public static class ModuleInitializer
+{
+    [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+    static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)]string lpFileName);
+    /// <summary>
+    /// Initializes the module.
+    /// </summary>
+    public static void Initialize()
+    {
+        if (!Environment.Is64BitProcess)
+        {
+            var handle = LoadLibrary("x86/RakNet.dll");
+        }
+        else
+        {
+            var handle = LoadLibrary("x64/RakNet.dll");
+        }
+    }
+}
--- DependentExtensions/Swig/SwigWindowsCSharpSample/SwigTestApp/TestMain.cs
+++ DependentExtensions/Swig/SwigWindowsCSharpSample/SwigTestApp/TestMain.cs
@@ -7,113 +7,27 @@ using System.Threading;
 using System.Timers;
 using System.IO;
 using RakNet;
+using System.Runtime.InteropServices;
 
 namespace SwigTestApp
 {
     class TestMain
     {
+      
+
         static void Main(string[] args)
         {
-            if (!File.Exists("RakNet.dll"))
-            {
-                Console.WriteLine("The SWIG build of the DLL has not been copied to the executable directory\nCopy from Swig/SwigWindowsCSharpSample/SwigTestApp/bin/X86/Debug/RakNet.dll to\nSwigWindowsCSharpSample/SwigTestApp/bin/Debug/RakNet.dll\nPress enter to quit.");
-                Console.Read();
-                return;
-            }
+            //if (!File.Exists("RakNet.dll"))
+            //{
+            //    Console.WriteLine("The SWIG build of the DLL has not been copied to the executable directory\nCopy from Swig/SwigWindowsCSharpSample/SwigTestApp/bin/X86/Debug/RakNet.dll to\nSwigWindowsCSharpSample/SwigTestApp/bin/Debug/RakNet.dll\nPress enter to quit.");
+            //    Console.Read();
+            //    return;
+            //}
 
-            try
-            {
-                RakString dllCallTest = new RakString();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("DLL issue\nAdd SwigOutput/CplusDLLIncludes/RakNetWrap.cxx to the project\nDLL_Swig/RakNet.sln and rebuild.\nPress enter to quit.");
-                Console.Read();
-                return;
-            }
-
-            Packet testPacket;
-            int loopNumber;
-            BitStream stringTestSendBitStream = new BitStream();
-            BitStream rakStringTestSendBitStream = new BitStream();
-            BitStream receiveBitStream = new BitStream();
-            String holdingString;
-            TimeSpan startTimeSpan;
-            RakString rakStringTest = new RakString();
-
-            RakPeerInterface testClient = RakPeer.GetInstance();
             
-            testClient.Startup(1, new SocketDescriptor(60000, "127.0.0.1"), 1);
 
-            RakPeerInterface testServer = RakPeer.GetInstance();
-            testServer.Startup(1, new SocketDescriptor(60001, "127.0.0.1"), 1);
-            testServer.SetMaximumIncomingConnections(1);
 
-            Console.WriteLine("Send and receive loop using BitStream.\nBitStream read done into RakString");
-
-            testClient.Connect("127.0.0.1", 60001, "", 0);
-
-            String sendString = "The test string";
-            stringTestSendBitStream.Write((byte)DefaultMessageIDTypes.ID_USER_PACKET_ENUM);
-            stringTestSendBitStream.Write(sendString);
-
-            RakString testRakString = new RakString("Test RakString");
-            rakStringTestSendBitStream.Write((byte)DefaultMessageIDTypes.ID_USER_PACKET_ENUM);
-            rakStringTestSendBitStream.Write(testRakString);
-
-            startTimeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1));
-            loopNumber = 0;
-
-            while (startTimeSpan.TotalSeconds + 5 > (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds)
-            {
-                testPacket = testServer.Receive();
-                if (testPacket != null && testPacket.data[0] == (byte)DefaultMessageIDTypes.ID_USER_PACKET_ENUM)
-                {
-                    receiveBitStream.Reset();
-                    receiveBitStream.Write(testPacket.data, testPacket.length);
-                    receiveBitStream.IgnoreBytes(1);
-                    receiveBitStream.Read(rakStringTest);
-                    Console.WriteLine("Loop number: " + loopNumber + "\nData: " + rakStringTest.C_String());
-                }
-                testServer.DeallocatePacket(testPacket);
-                loopNumber++;
-                System.Threading.Thread.Sleep(50);
-                testClient.Send(rakStringTestSendBitStream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, (char)0, new AddressOrGUID(new SystemAddress("127.0.0.1", 60001)), false);
-            }
-
-            Console.WriteLine("String send and receive loop using BitStream.\nBitStream read done into String");
-
-            SystemAddress[] remoteSystems;
-            ushort numberOfSystems=1;
-            testServer.GetConnectionList(out remoteSystems, ref numberOfSystems);
-
-            startTimeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1));
-            loopNumber = 0;
-            while (startTimeSpan.TotalSeconds + 5 > (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds)
-            {
-                testPacket = testServer.Receive();
-                if (testPacket != null && testPacket.data[0] == (byte)DefaultMessageIDTypes.ID_USER_PACKET_ENUM)
-                {
-                    receiveBitStream.Reset();
-                    receiveBitStream.Write(testPacket.data, testPacket.length);
-                    receiveBitStream.IgnoreBytes(1);
-                    receiveBitStream.Read(out holdingString);
-                    Console.WriteLine("Loop number: " + loopNumber + "\nData: " + holdingString);
-                }
-                testServer.DeallocatePacket(testPacket);
-                loopNumber++;
-                System.Threading.Thread.Sleep(50);
-                SystemAddress sa = RakNet.RakNet.UNASSIGNED_SYSTEM_ADDRESS;
-                testClient.Send(stringTestSendBitStream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, (char)0, new AddressOrGUID(new SystemAddress("127.0.0.1", 60001)), false);
-            }
-            //If RakString is not freed before program exit it will crash
-            rakStringTest.Dispose();
-            testRakString.Dispose();
-
-            RakPeer.DestroyInstance(testClient);
-            RakPeer.DestroyInstance(testServer);
-            Console.WriteLine("Demo complete. Press Enter.");
-            Console.Read();
+            Tests.Run();
         }
     }
 
@@ -424,5 +338,11 @@ namespace SwigTestApp
             RakPeerInterface.DestroyInstance(rakPeer);
         }
 #endif
-
+    public class Tests
+    {
+        public static void Run()
+        {
+            
+        }
+    }
 }
